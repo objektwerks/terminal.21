2249d0f17011da4b15adf9e11d046762f53c0c59
objektwerks <objektwerks@runbox.com>
2024-02-07 10:38:04 -0500

    edited project.scala

--- project.scala
+++ project.scala
@@ -4,5 +4,4 @@
 //> using dep commons-io:commons-io:2.15.1
 
 //> using dep io.github.kostaskougios::terminal21-ui-std:0.21
-//> using dep io.github.kostaskougios::terminal21-nivo:0.21
-//> using dep io.github.kostaskougios::terminal21-mathjax:0.21
+//> using dep io.github.kostaskougios::terminal21-server:0.21
