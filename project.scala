fb23e361a44a57339cc2d91f2691150e96cea4ec
objektwerks <objektwerks@runbox.com>
2024-02-07 10:59:17 -0500

    readded project.scala

--- project.scala
+++ project.scala
@@ -1,7 +1,5 @@
 //> using jvm "21"
 //> using scala 3
 
-//> using dep commons-io:commons-io:2.15.1
-
 //> using dep io.github.kostaskougios::terminal21-ui-std:0.21
 //> using dep io.github.kostaskougios::terminal21-server:0.21
