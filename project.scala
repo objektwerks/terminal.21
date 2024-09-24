1bfed163bc830e2eb31ddf846f010d0eccfa35e0
Objektwerks <objektwerks@runbox.com>
2024-09-24 09:43:39 -0400

    Update project.scala

--- project.scala
+++ project.scala
@@ -1,5 +1,5 @@
-//> using jvm "22"
-//> using scala 3.5.1-RC2
+//> using jvm "23"
+//> using scala 3.5.1
 
 //> using dep io.github.kostaskougios::terminal21-ui-std:0.30
 //> using dep io.github.kostaskougios::terminal21-server:0.30
